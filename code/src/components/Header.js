083e5ab009410c94042c4f64a902f3af497f7eeb
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-04-04 00:41:45 +0200

    added header

---
+++ code/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../css/header.css'
+import { Title } from './Title'
+
+
+export const Header = () => {
+
+  return (
+    <>
+      <header>
+        <div>
+          <h1>Portfolio: <span>Nathalie Rautell</span></h1>
+          <Title />
+        </div>
+      </header>
+    </>
+  )
+}
+
+
+
