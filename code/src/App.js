ecb07b46a4ace2d3277ad1fab0bb5afd9efefe02
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-04-05 18:25:16 +0200

    added icons

--- code/src/App.js
+++ code/src/App.js
@@ -5,9 +5,9 @@ import { About } from './pages/About'
 import { Contact } from './pages/Contact'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faChevronCircleDown, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faEnvelopeOpen, faAngleDown, faAngleUp, faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons'
 
-library.add(fab, faChevronCircleDown, faAngleDown)
+library.add(fab, faEnvelope, faEnvelopeOpen, faAngleDown, faAngleUp, faAngleDoubleUp)
 
 export const App = () => {
   return (
--- code/src/components/Navbar.js
+++ code/src/components/Navbar.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import "../css/navbar.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export const Navbar = () => {
   return (
     <>
       <nav>
         <ul>
-          <li>Home</li>
+          <div className="icon-container">
+            <a id="top">
+              <FontAwesomeIcon id="topOne" icon="angle-up" />
+              <FontAwesomeIcon id="topTwo" icon="angle-double-up" />
+            </a>
+            <a id="envelope">
+              <FontAwesomeIcon id="envelopeClose" icon="envelope" />
+              <FontAwesomeIcon id="envelopeOpen" icon="envelope-open" />
+            </a>
+          </div>
           <li>About</li>
           <li>Tech</li>
           <li>Projects</li>
