d2a14c9b0a1aaa301a684efbdc246276d0ce3fb8
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-04-09 17:45:44 +0200

    added more content into the pages

--- code/src/pages/Skills.js
+++ code/src/pages/Skills.js
@@ -8,37 +8,43 @@ export const Skills = () => {
       <main>
         <section id="tech">
           <h2>Tech</h2>
-          <p> HTML ⎪ CSS ⎪ JavaScript ⎪ ES6 ⎪ JSX ⎪ React ⎪ Redux ⎪ Node.js ⎪ Mongo DB ⎪ Web Accessibility ⎪ API ⎪ JSON ⎪ Git </p>
+          <p> HTML ⎪ CSS ⎪ JavaScript ⎪ ES6 ⎪ JSX ⎪ React ⎪ Redux ⎪ Node.js ⎪ Mongo DB ⎪ Web Accessibility ⎪ API ⎪ JSON ⎪ Cross browsing </p>
         </section>
         <section id="skills">
-          <h2>Skills</h2>
+          <h2>Knowledge</h2>
           <article>
             <ul>
               <h3>Code</h3>
               <li>HTML5</li>
+              <li>Semantic-UI</li>
               <li>CSS3</li>
               <li>JavaScript ES6</li>
-              <li>React</li>
+              <li>React.js</li>
+              <li>React Native</li>
               <li>Git</li>
               <li>Node.js</li>
             </ul>
             <ul>
               <h3>Toolbox</h3>
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>JavaScript ES6</li>
-              <li>React</li>
-              <li>Git</li>
-              <li>Node.js</li>
+              <li>Visual Studio Code</li>
+              <li>Github</li>
+              <li>Chrome Dev Tool</li>
+              <li>Firefox Dev Tool</li>
+              <li>Npm</li>
+              <li>Postman</li>
+              <li>Gapplin</li>
+              <li>JIRA</li>
+              <li>Figma</li>
+              <li>Slack</li>
             </ul>
             <ul>
-              <h3>More</h3>
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>JavaScript ES6</li>
-              <li>React</li>
-              <li>Git</li>
-              <li>Node.js</li>
+              <h3>Other</h3>
+              <li>Life insurance</li>
+              <li>Money laundry</li>
+              <li>Investment</li>
+              <li>E-commerce</li>
+              <li>Fraud</li>
+              <li>POS</li>
             </ul>
           </article>
         </section>
