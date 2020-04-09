e640fecef43df26e7b4ba5d61e2afd631a5c79ac
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-04-09 18:12:38 +0200

    added icons in footer

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "../css/footer.css"
 
 export const Footer = () => {
@@ -10,6 +11,18 @@ export const Footer = () => {
         <p>Nathalie Rautell</p>
         <a href="mailto: nathalie.rautell@gmail.com">nathalie.rautell@gmail.com</a>
         <a href="tel:+46724303362">072-430 33 62</a>
+        <div className="logos-container">
+          <a className="logos" target="_blank" rel="noopener noreferrer" role="button" aria-pressed="false" tabIndex="0" href="https://github.com/rautellin">
+            <FontAwesomeIcon icon={['fab', 'github-alt']} />
+          </a>
+          <a className="logos" target="_blank" rel="noopener noreferrer" role="button" aria-pressed="false" tabIndex="0" href="https://www.linkedin.com/in/nathalierautell/">
+            <FontAwesomeIcon icon={['fab', 'linkedin-in']} />
+          </a>
+          <a className="logos" target="_blank" rel="noopener noreferrer" role="button" aria-pressed="false" tabIndex="0"
+            href="https://stackoverflow.com/users/11282492/nathalie?tab=profile">
+            <FontAwesomeIcon icon={['fab', 'stack-overflow']} />
+          </a>
+        </div>
       </footer>
     </>
   )
