8a718435acd4799cd443ec9d2ffb25a3cf27fcd9
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-04-11 21:49:00 +0200

    small changes

--- code/src/index.js
+++ code/src/index.js
@@ -9,8 +9,19 @@ const hamburger = document.querySelector('.hamburger')
 const navlinks = document.querySelector('.nav-links')
 const links = document.querySelectorAll('.nav-links li')
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('mousedown', (event) => {
+  event.preventDefault()
   navlinks.classList.toggle('open')
-})
+}, { passive: false })
+
+hamburger.addEventListener('touchstart', (event) => {
+  event.preventDefault()
+  navlinks.classList.toggle('open')
+}, { passive: false })
+
+hamburger.addEventListener('touchend', (event) => {
+  event.preventDefault()
+}, { passive: false })
+
 
 
