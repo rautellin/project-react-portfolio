9766b82a635dcc3fb8e579f6e3938cf2ee395bf6
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-04-25 20:26:31 +0200

    changed about text

--- code/src/pages/About.js
+++ code/src/pages/About.js
@@ -31,8 +31,8 @@ export const About = ({ container, item, style }) => {
         <motion.section style={sectionStyle} variants={item} className='about' id='about'>
           <motion.div variants={item}>
             <p>You either find me typing on a keyboard or at the gym somewhere between the weights. So basically I'm always building something, if not websites and applications then physical strength. </p>
-            <p>My love for code goes way back, and after years apart our roads crossed again. I finally realized its ability to stimulate my mind and curiosity and now we’re finally living happily ever after. <br /><br />
-            And as with any relationship, conflicts are inevitable. But that’s the true way of growing together. At the same time, the beauty of this endless path of learning is to be found in those challenges. </p>
+            <p>My love for coding goes way back. After years apart, our paths once again crossed. I finally realized its ability to stimulate my mind and curiosity, and now we’re living happily ever after. <br /><br />
+            As with any relationship, there are conflicts. But that’s the only way to grow. At the same time, the beauty of this endless path of learning is found in these challenges. </p>
           </motion.div>
         </motion.section>
       </motion.div>
